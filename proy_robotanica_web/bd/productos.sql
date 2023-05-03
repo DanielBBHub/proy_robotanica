5e4c42e3ccc663711670e55496b0774bef1fa795
Daniel Benavides <ruxzfly@gmail.com>
2023-05-03 12:04:56 +0200

    T15 - T16 Implementacion de BD Productos e Invernadero

--- proy_robotanica_web/bd/productos.sql
+++ proy_robotanica_web/bd/productos.sql
@@ -1,11 +1,9 @@
+create table Productos(
+	idInvernadero    varchar(9)    PRIMARY KEY,
+    productos   varchar(50)    not null,
+    madurez    varchar(50)    not null,
+    fechaPlantacion    date    not null
+);
 
-DROP TABLE productos;
-CREATE TABLE productos{
-    primary key idInvernadero,
-    productos varchar(50) not null,
-    madurez varchar(50) not null,
-    fechaPlantacion date not null,
-}
-
-insert into productos values('8AInvernadero1','Tomate,Pepino', 'Verde,Verde', '2020-01-01,2020-01-03');
-insert into productos values('72AInvernadero2','Tomate,Calabaza', 'Verde,Verde', '2020-01-01,2020-01-03');
+insert into Productos values('78AInvernadero1','Tomate,Pepino', 'Verde,Verde', '2020-01-01,2020-01-03');
+insert into Productos values('72AInvernadero2','Tomate,Calabaza', 'Verde,Verde', '2020-01-01,2020-01-03');
